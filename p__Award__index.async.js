1f169fe2e68d6916bbf35cc152fff31283c20c56
shuaijiumei <740514874@qq.com>
2024-02-23 16:12:19 +0800

    Updates

---
+++ p__Award__index.async.js
@@ -0,0 +1,34 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[317],{4675:function(t,l,s){s.d(l,{Z:function(){return x}});var c=s(34200),o=s(88029),e=s(2777),d=s(49161),a=s(26755),r=s(48671),h=s(90136),v=s(73917),i=s(49721),m=s.p+"static/logo.65f60def.png",j=`**MSC Student** (MAIR)   
+in CUHKSZ.  
+Looking for a **Phd position**  
+in \`Software Engineering\`.
+Interested in **LLM**.`,n=s(36071),x=function(){return(0,n.jsxs)("div",{className:"sidebar",children:[(0,n.jsxs)("div",{className:"profile",children:[(0,n.jsx)(o.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:m}),(0,n.jsxs)("div",{className:"info",children:[(0,n.jsx)("div",{className:"name",children:i.name}),(0,n.jsx)("div",{className:"shortInfo",children:(0,n.jsx)(e.U,{children:j})})]})]}),(0,n.jsxs)("div",{className:"link",children:[(0,n.jsxs)("div",{children:[(0,n.jsx)(d.Z,{className:"icon"}),i.location]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(a.Z,{className:"icon"}),(0,n.jsx)("a",{href:i.github,children:"Github"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(r.Z,{className:"icon"}),(0,n.jsx)("a",{href:i==null?void 0:i.twitter,children:"Twitter"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(h.Z,{className:"icon"}),(0,n.jsx)("a",{href:i.email,children:"Email"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(v.Z,{className:"icon"}),(0,n.jsx)("a",{href:i==null?void 0:i.googleScholar,children:"Google Scholar"})]})]})]})}},69312:function(t,l,s){s.r(l),s.d(l,{default:function(){return r}});var c=s(34200),o=`# \u5927\u5BB6\u597D\uFF0C\u8FD9\u662F\u6D4B\u8BD5\u7248\u7684Markdown\u6587\u6863
+
+\u8FD9\u662F\u4E00\u6BB5\u793A\u4F8B\u6587\u672C\uFF0C\u4EE5\u4E0B\u662F\u4E00\u4E9BMarkdown\u7684\u57FA\u672C\u8BED\u6CD5\u793A\u4F8B\u3002
+
+## \u5C0F\u6807\u9898 1
+
+- \u5217\u8868\u98791
+- \u5217\u8868\u98792
+- \u5217\u8868\u98793
+
+### \u5C0F\u5C0F\u6807\u9898 1
+
+\u8FD9\u662F\u4E00\u6BB5*\u659C\u4F53*\u6587\u672C\uFF0C\u8FD9\u662F\u4E00\u6BB5**\u7C97\u4F53**\u6587\u672C\u3002
+
+#### \u5C0F\u5C0F\u5C0F\u6807\u9898 1
+
+1. \u6709\u5E8F\u5217\u8868\u98791
+2. \u6709\u5E8F\u5217\u8868\u98792
+3. \u6709\u5E8F\u5217\u8868\u98793
+
+##### \u5C0F\u5C0F\u5C0F\u5C0F\u6807\u9898 1
+
+> \u8FD9\u662F\u4E00\u4E2A\u5F15\u7528\u3002
+
+###### \u6700\u5C0F\u6807\u9898
+
+\`\`\`python
+# \u8FD9\u662F\u4E00\u6BB5\u4EE3\u7801
+print("Hello, Markdown!")
+`,e=s(4675),d=s(2777),a=s(36071),r=function(){return(0,a.jsxs)("div",{className:"page",children:[(0,a.jsx)(e.Z,{}),(0,a.jsxs)("div",{className:"content",children:[(0,a.jsx)("div",{className:"title",children:"Awards"}),(0,a.jsx)("div",{className:"markdown",children:(0,a.jsx)(d.U,{children:o})})]})]})}},49721:function(t){t.exports=JSON.parse('{"name":"Boyin Tan","shortInfo":"**Msc Student** at Chinese University of Hong Kong, Shenzhen","location":"ShenZhen, China","email":"BoyinTan1221@gmail.com","github":"https://github.com/shuaijiumei","twitter":"https://twitter.com/BoyinTan","googleScholar":"https://scholar.google.com/citations?user=3z3z3z4AAAAJ&hl=en","showBar":["aboutme","publications","awards","services","cv"]}')}}]);
---
+++ p__Services__index.async.js
@@ -0,0 +1,34 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[767],{4675:function(l,e,s){s.d(e,{Z:function(){return x}});var r=s(34200),t=s(88029),o=s(2777),d=s(49161),a=s(26755),c=s(48671),h=s(90136),v=s(73917),i=s(49721),m=s.p+"static/logo.65f60def.png",j=`**MSC Student** (MAIR)   
+in CUHKSZ.  
+Looking for a **Phd position**  
+in \`Software Engineering\`.
+Interested in **LLM**.`,n=s(36071),x=function(){return(0,n.jsxs)("div",{className:"sidebar",children:[(0,n.jsxs)("div",{className:"profile",children:[(0,n.jsx)(t.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:m}),(0,n.jsxs)("div",{className:"info",children:[(0,n.jsx)("div",{className:"name",children:i.name}),(0,n.jsx)("div",{className:"shortInfo",children:(0,n.jsx)(o.U,{children:j})})]})]}),(0,n.jsxs)("div",{className:"link",children:[(0,n.jsxs)("div",{children:[(0,n.jsx)(d.Z,{className:"icon"}),i.location]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(a.Z,{className:"icon"}),(0,n.jsx)("a",{href:i.github,children:"Github"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(c.Z,{className:"icon"}),(0,n.jsx)("a",{href:i==null?void 0:i.twitter,children:"Twitter"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(h.Z,{className:"icon"}),(0,n.jsx)("a",{href:i.email,children:"Email"})]}),(0,n.jsxs)("div",{children:[(0,n.jsx)(v.Z,{className:"icon"}),(0,n.jsx)("a",{href:i==null?void 0:i.googleScholar,children:"Google Scholar"})]})]})]})}},68442:function(l,e,s){s.r(e),s.d(e,{default:function(){return c}});var r=s(34200),t=`# \u6807\u9898 \u5927\u5BB6\u597D
+
+\u8FD9\u662F\u4E00\u6BB5\u793A\u4F8B\u6587\u672C\uFF0C\u4EE5\u4E0B\u662F\u4E00\u4E9BMarkdown\u7684\u57FA\u672C\u8BED\u6CD5\u793A\u4F8B\u3002
+
+## \u5C0F\u6807\u9898 1
+
+- \u5217\u8868\u98791
+- \u5217\u8868\u98792
+- \u5217\u8868\u98793
+
+### \u5C0F\u5C0F\u6807\u9898 1
+
+\u8FD9\u662F\u4E00\u6BB5*\u659C\u4F53*\u6587\u672C\uFF0C\u8FD9\u662F\u4E00\u6BB5**\u7C97\u4F53**\u6587\u672C\u3002
+
+#### \u5C0F\u5C0F\u5C0F\u6807\u9898 1
+
+1. \u6709\u5E8F\u5217\u8868\u98791
+2. \u6709\u5E8F\u5217\u8868\u98792
+3. \u6709\u5E8F\u5217\u8868\u98793
+
+##### \u5C0F\u5C0F\u5C0F\u5C0F\u6807\u9898 1
+
+> \u8FD9\u662F\u4E00\u4E2A\u5F15\u7528\u3002
+
+###### \u6700\u5C0F\u6807\u9898
+
+\`\`\`python
+# \u8FD9\u662F\u4E00\u6BB5\u4EE3\u7801
+print("Hello, Markdown!")
+`,o=s(4675),d=s(2777),a=s(36071),c=function(){return(0,a.jsxs)("div",{className:"page",children:[(0,a.jsx)(o.Z,{}),(0,a.jsxs)("div",{className:"content",children:[(0,a.jsx)("div",{className:"title",children:"Services"}),(0,a.jsx)("div",{className:"markdown",children:(0,a.jsx)(d.U,{children:t})})]})]})}},49721:function(l){l.exports=JSON.parse('{"name":"Boyin Tan","shortInfo":"**Msc Student** at Chinese University of Hong Kong, Shenzhen","location":"ShenZhen, China","email":"BoyinTan1221@gmail.com","github":"https://github.com/shuaijiumei","twitter":"https://twitter.com/BoyinTan","googleScholar":"https://scholar.google.com/citations?user=3z3z3z4AAAAJ&hl=en","showBar":["aboutme","publications","awards","services","cv"]}')}}]);
