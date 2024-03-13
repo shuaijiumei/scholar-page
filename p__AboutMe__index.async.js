722b0b4a64ddc6eb06cb037d1b489ad8cf274d47
shuaijiumei <740514874@qq.com>
2024-03-13 16:07:55 +0800

    Updates

--- p__AboutMe__index.async.js
+++ p__AboutMe__index.async.js
@@ -1,33 +1,23 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[323],{4675:function(o,l,n){n.d(l,{Z:function(){return g}});var r=n(34200),t=n(48310),e=n(88029),d=n(2777),i=n(93486),c=n.n(i),h=n(49161),v=n(26755),m=n(48671),j=n(90136),x=n(73917),a=n(49721),u=n.p+"static/logo.65f60def.png",f=`**MSC Student** (MAIR)   
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[323],{4675:function(a,s,n){n.d(s,{Z:function(){return x}});var d=n(34200),t=n(48310),r=n(88029),l=n(2777),o=n(93486),c=n.n(o),h=n(49161),m=n(26755),v=n(48671),f=n(90136),u=n(73917),i=n(49721),g=n.p+"static/logo.65f60def.png",j=`**MSC Student** (MAIR)   
 in CUHKSZ.  
 Looking for a **Phd position**  
 in \`Software Engineering\`.
-Interested in **LLM**.`,s=n(36071),g=function(){var N=function(S){S.preventDefault(),c()(a.email),t.ZP.success("Email copied to clipboard",1)};return(0,s.jsxs)("div",{className:"sidebar",children:[(0,s.jsxs)("div",{className:"profile",children:[(0,s.jsx)(e.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:u}),(0,s.jsxs)("div",{className:"info",children:[(0,s.jsx)("div",{className:"name",children:a.name}),(0,s.jsx)("div",{className:"shortInfo",children:(0,s.jsx)(d.U,{children:f})})]})]}),(0,s.jsxs)("div",{className:"link",children:[(0,s.jsxs)("div",{children:[(0,s.jsx)(h.Z,{className:"icon"}),a.location]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(v.Z,{className:"icon"}),(0,s.jsx)("a",{href:a.github,children:"Github"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(m.Z,{className:"icon"}),(0,s.jsx)("a",{href:a==null?void 0:a.twitter,children:"Twitter"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(j.Z,{className:"icon"}),(0,s.jsx)("a",{onClick:N,href:"",children:"Email"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(x.Z,{className:"icon"}),(0,s.jsx)("a",{href:a==null?void 0:a.googleScholar,children:"Google Scholar"})]})]})]})}},47273:function(o,l,n){n.r(l),n.d(l,{default:function(){return c}});var r=n(34200),t=`
-\u8FD9\u662F\u4E00\u6BB5\u6F66\u8349\u7684\u81EA\u6211\u4ECB\u7ECD\u8FD9\u662F\u4E00\u6BB5\u6F66\u8349\u7684\u81EA\u6211\u4ECB\u7ECD\u8FD9\u662F\u4E00\u6BB5\u6F66\u8349\u7684\u81EA\u6211\u4ECB\u7ECD\u8FD9\u662F\u4E00\u6BB5\u6F66\u8349\u7684\u81EA\u6211\u4ECB\u7ECD\u8FD9\u662F\u4E00\u6BB5\u6F66\u8349\u7684\u81EA\u6211\u4ECB\u7ECD\u8FD9\u662F\u4E00\u6BB5\u6F66\u8349\u7684\u81EA\u6211\u4ECB\u7ECD\u8FD9\u662F\u4E00\u6BB5\u6F66\u8349\u7684\u81EA\u6211\u4ECB\u7ECD\u8FD9\u662F\u4E00\u6BB5\u6F66\u8349\u7684\u81EA\u6211\u4ECB\u7ECD\u8FD9\u662F\u4E00\u6BB5\u6F66\u8349\u7684\u81EA\u6211\u4ECB\u7ECD\u8FD9\u662F\u4E00\u6BB5\u6F66\u8349\u7684\u81EA\u6211\u4ECB\u7ECD\u8FD9\u662F\u4E00\u6BB5\u6F66\u8349\u7684\u81EA\u6211\u4ECB\u7ECD
+Interested in **AIOps**, **Program Analyse**.`,e=n(36071),x=function(){var p=function(S){S.preventDefault(),c()(i.email),t.ZP.success("Email copied to clipboard",1)};return(0,e.jsxs)("div",{className:"sidebar",children:[(0,e.jsxs)("div",{className:"profile",children:[(0,e.jsx)(r.C,{size:{xs:48,sm:64,md:80,lg:128,xl:160,xxl:200},src:g}),(0,e.jsxs)("div",{className:"info",children:[(0,e.jsx)("div",{className:"name",children:i.name}),(0,e.jsx)("div",{className:"shortInfo",children:(0,e.jsx)(l.U,{children:j})})]})]}),(0,e.jsxs)("div",{className:"link",children:[(0,e.jsxs)("div",{children:[(0,e.jsx)(h.Z,{className:"icon"}),i.location]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(m.Z,{className:"icon"}),(0,e.jsx)("a",{href:i.github,children:"Github"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(v.Z,{className:"icon"}),(0,e.jsx)("a",{href:i==null?void 0:i.twitter,children:"Twitter"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(f.Z,{className:"icon"}),(0,e.jsx)("a",{onClick:p,href:"",children:"Email"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(u.Z,{className:"icon"}),(0,e.jsx)("a",{href:i==null?void 0:i.googleScholar,children:"Google Scholar"})]})]})]})}},47273:function(a,s,n){n.r(s),n.d(s,{default:function(){return c}});var d=n(34200),t=`
+Boyin Tan is a 1st-year master student at The Chinese University of HongKong,(ShenZhen), under the supervision of [Prof.Pinjia He](https://pinjiahe.github.io/).  Previously, He received Bachelor\u2019s degree in Software Engineering from **UESTC** (University of Electronic Science and Technology of China) in 2023.  
+My Research interests include LLM for code, program analysis and AIOps.   
+I have very rich development experience and have been independently responsible for project development many times including website, mini Program and back-end systems.  
+**Now I am looking for a PHD position, feel free to drop me an email if you are interested in my research or just want to talk with me\u{1F606}**
 
-## \u5C0F\u6807\u9898 1
+## \u{1F4D6} Educations
++ 2023.09 ~ **Present** master student, Chinese University of Hong Kong,(ShenZhen)
++ 2019.09 ~ 2023.06 Bachelor degree, University of Electronic Science and Technology of China
+## \u{1F3C3} Experiences
++ 2022.01 ~ 2022.06 Front-end Development Dntern Engineer, Ant Group **(Team maintains the Antd and Umi)**
++ 2021.01 \uFF5E 2022.01 Studio Chairman of XingSanHao Club of UESTC
++ 2020.09 ~ 2022.01 Football Team Captain of School of Information and Software Engineering of UESTC
 
-- \u5217\u8868\u98791
-- \u5217\u8868\u98792
-- \u5217\u8868\u98793
+## \u{1F396} Awards
 
-### \u5C0F\u5C0F\u6807\u9898 1
-
-\u8FD9\u662F\u4E00\u6BB5*\u659C\u4F53*\u6587\u672C\uFF0C\u8FD9\u662F\u4E00\u6BB5**\u7C97\u4F53**\u6587\u672C\u3002
-
-#### \u5C0F\u5C0F\u5C0F\u6807\u9898 1
-
-1. \u6709\u5E8F\u5217\u8868\u98791
-2. \u6709\u5E8F\u5217\u8868\u98792
-3. \u6709\u5E8F\u5217\u8868\u98793
-
-##### \u5C0F\u5C0F\u5C0F\u5C0F\u6807\u9898 1
-
-> \u8FD9\u662F\u4E00\u4E2A\u5F15\u7528\u3002
-
-###### \u6700\u5C0F\u6807\u9898
-
-\`\`\`python
-# \u8FD9\u662F\u4E00\u6BB5\u4EE3\u7801
-print("Hello, Markdown!")
-`,e=n(4675),d=n(2777),i=n(36071),c=function(){return(0,i.jsxs)("div",{className:"page",children:[(0,i.jsx)(e.Z,{}),(0,i.jsxs)("div",{className:"content",children:[(0,i.jsx)("div",{className:"title",children:"About Me"}),(0,i.jsx)("div",{className:"markdown",children:(0,i.jsx)(d.U,{children:t})})]})]})}},49721:function(o){o.exports=JSON.parse('{"name":"Boyin Tan","location":"ShenZhen, China","email":"BoyinTan1221@gmail.com","github":"https://github.com/shuaijiumei","twitter":"https://twitter.com/BoyinTan","googleScholar":"https://scholar.google.com/citations?user=3z3z3z4AAAAJ&hl=en","showBar":["aboutme","publications","awards","services","cv"]}')}}]);
+- **First Prize** of Sichuan Province in China Computer Design Competition (2021)
+- "Innovation, Creativity and Entrepreneurship Competition" Sichuan Province Second Prize (2021)
+- Second Prize in the Southwest Division of College WeChat Mini Program (2020,2019)`,r=n(4675),l=n(2777),o=n(36071),c=function(){return(0,o.jsxs)("div",{className:"page",children:[(0,o.jsx)(r.Z,{}),(0,o.jsxs)("div",{className:"content",children:[(0,o.jsx)("div",{className:"title",children:"About Me"}),(0,o.jsx)("div",{className:"markdown",children:(0,o.jsx)(l.U,{children:t})})]})]})}},49721:function(a){a.exports=JSON.parse('{"name":"Boyin Tan","location":"ShenZhen, China","email":"BoyinTan1221@gmail.com","github":"https://github.com/shuaijiumei","twitter":"https://twitter.com/BoyinTan","googleScholar":"https://scholar.google.com/citations?user=3z3z3z4AAAAJ&hl=en","showBar":["aboutme","publications","cv"]}')}}]);
