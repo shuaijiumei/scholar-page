1a7b3c1b823e4cf3a71a43e2db2cc459bfbd1547
AL-Bench <shuaijiumei@126.com>
2025-04-17 10:56:12 +0800

    Updates

---
+++ p__AboutMe__index.299aa572.async.js
@@ -0,0 +1,23 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[323],{65273:function(s,o,n){n.d(o,{Z:function(){return N}});var h=n(5574),r=n.n(h),t=n(67294),l=n(45803),a=n(34425),c=n(48885),u=n(20640),g=n.n(u),j=n(33862),x=n(1210),S=n(85093),y=n(10524),p=n(14079),i=n(49721),v=n.p+"static/logo.65f60def.png",C=`**Master Student** in CUHKSZ.  
+Looking for a **Phd position**  
+in \`Software Engineering\`.
+Interested in **AI4SE**, **Program Analyse**.`,e=n(85893),N=function(){var E=function(P){P.preventDefault(),g()(i.email),l.ZP.success("Email copied to clipboard",1)},d=t.useRef(null),T=t.useState(0),m=r()(T,2),I=m[0],A=m[1];return t.useEffect(function(){if(d.current){var f=d.current.offsetWidth*5+30;A(f)}},[v]),(0,e.jsxs)("div",{className:"sidebar",children:[(0,e.jsxs)("div",{className:"profile",children:[(0,e.jsx)(a.Z,{ref:d,className:"avatar",size:{xs:48,sm:64,md:80,lg:64,xl:160,xxl:150},src:v}),(0,e.jsxs)("div",{className:"info",children:[(0,e.jsx)("div",{className:"name",children:i.name}),(0,e.jsx)("div",{className:"shortInfo",children:(0,e.jsx)(c.UG,{children:C})})]})]}),(0,e.jsxs)("div",{className:"link",children:[(0,e.jsxs)("div",{children:[(0,e.jsx)(j.Z,{className:"icon"}),i.location]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(x.Z,{className:"icon"}),(0,e.jsx)("a",{href:i.github,children:"Github"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(S.Z,{className:"icon"}),(0,e.jsx)("a",{href:i==null?void 0:i.twitter,children:"Twitter"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(y.Z,{className:"icon"}),(0,e.jsx)("a",{onClick:E,href:"",children:"Email"})]}),(0,e.jsxs)("div",{children:[(0,e.jsx)(p.Z,{className:"icon"}),(0,e.jsx)("a",{href:i==null?void 0:i.googleScholar,children:"Google Scholar"})]})]})]})}},92041:function(s,o,n){n.r(o),n.d(o,{default:function(){return c}});var h=n(67294),r=`
+Boyin Tan is a 2nd-year master student at **The Chinese University of HongKong, (ShenZhen)**, under the supervision of [Prof.Pinjia He](https://pinjiahe.github.io/).  Previously, He received Bachelor\u2019s degree in Software Engineering from **UESTC** (University of Electronic Science and Technology of China) in 2023.  
+My Research interests include LLM for code, program analysis and AIOps.   
+I have very rich development experience and have been independently responsible for project development many times including website, mini Program and back-end systems.  
+**Now I am looking for a PHD position, feel free to drop me an email if you are interested in my research or just want to talk with me\u{1F606}**
+
+## \u{1F4D6} Educations
++ 2023.09 ~ **Present** master student, Chinese University of Hong Kong,(ShenZhen)
++ 2019.09 ~ 2023.06 Bachelor degree, University of Electronic Science and Technology of China
+## \u{1F3C3} Experiences
++ 2024.06 ~ **Present** Intern, Tencent, Shenzhen
++ 2022.01 ~ 2022.06 Front-end Development Intern Engineer, Ant Group **(Team maintains the Antd and Umi)**
++ 2021.01 ~ 2022.01 Studio Chairman of XingSanHao Club of UESTC
++ 2020.09 ~ 2022.01 Football Team Captain of School of Information and Software Engineering of UESTC
+
+## \u{1F396} Awards
+
+- **First Prize** of Sichuan Province in China Computer Design Competition (2021)
+- "Innovation, Creativity and Entrepreneurship Competition" Sichuan Province Second Prize (2021)
+- Second Prize in the Southwest Division of College WeChat Mini Program (2020,2019)`,t=n(65273),l=n(48885),a=n(85893),c=function(){return(0,a.jsxs)("div",{className:"page",children:[(0,a.jsx)(t.Z,{}),(0,a.jsxs)("div",{className:"content",children:[(0,a.jsx)("div",{className:"title",children:"About Me"}),(0,a.jsx)("div",{className:"markdown",children:(0,a.jsx)(l.UG,{children:r})})]})]})}},49721:function(s){s.exports=JSON.parse('{"name":"Boyin Tan","location":"ShenZhen, China","email":"BoyinTan1221@gmail.com","github":"https://github.com/shuaijiumei","twitter":"https://twitter.com/BoyinTan","googleScholar":"https://scholar.google.com/citations?hl=zh-CN&user=lvhoqu4AAAAJ","showBar":["aboutme","publications","cv"]}')}}]);
---
+++ p__Services__index.e61f37f1.async.js
@@ -0,0 +1,33 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[767],{65273:function(o,l,n){n.d(l,{Z:function(){return A}});var v=n(5574),e=n.n(v),i=n(67294),c=n(45803),t=n(34425),r=n(48885),u=n(20640),j=n.n(u),x=n(33862),g=n(1210),N=n(85093),S=n(10524),Z=n(14079),a=n(49721),h=n.p+"static/logo.65f60def.png",y=`**Master Student** in CUHKSZ.  
+Looking for a **Phd position**  
+in \`Software Engineering\`.
+Interested in **AI4SE**, **Program Analyse**.`,s=n(85893),A=function(){var O=function(B){B.preventDefault(),j()(a.email),c.ZP.success("Email copied to clipboard",1)},d=i.useRef(null),I=i.useState(0),m=e()(I,2),C=m[0],T=m[1];return i.useEffect(function(){if(d.current){var f=d.current.offsetWidth*5+30;T(f)}},[h]),(0,s.jsxs)("div",{className:"sidebar",children:[(0,s.jsxs)("div",{className:"profile",children:[(0,s.jsx)(t.Z,{ref:d,className:"avatar",size:{xs:48,sm:64,md:80,lg:64,xl:160,xxl:150},src:h}),(0,s.jsxs)("div",{className:"info",children:[(0,s.jsx)("div",{className:"name",children:a.name}),(0,s.jsx)("div",{className:"shortInfo",children:(0,s.jsx)(r.UG,{children:y})})]})]}),(0,s.jsxs)("div",{className:"link",children:[(0,s.jsxs)("div",{children:[(0,s.jsx)(x.Z,{className:"icon"}),a.location]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(g.Z,{className:"icon"}),(0,s.jsx)("a",{href:a.github,children:"Github"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(N.Z,{className:"icon"}),(0,s.jsx)("a",{href:a==null?void 0:a.twitter,children:"Twitter"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(S.Z,{className:"icon"}),(0,s.jsx)("a",{onClick:O,href:"",children:"Email"})]}),(0,s.jsxs)("div",{children:[(0,s.jsx)(Z.Z,{className:"icon"}),(0,s.jsx)("a",{href:a==null?void 0:a.googleScholar,children:"Google Scholar"})]})]})]})}},92244:function(o,l,n){n.r(l),n.d(l,{default:function(){return r}});var v=n(67294),e=`# \u6807\u9898 \u5927\u5BB6\u597D
+
+\u8FD9\u662F\u4E00\u6BB5\u793A\u4F8B\u6587\u672C\uFF0C\u4EE5\u4E0B\u662F\u4E00\u4E9BMarkdown\u7684\u57FA\u672C\u8BED\u6CD5\u793A\u4F8B\u3002
+
+## \u5C0F\u6807\u9898 1
+
+- \u5217\u8868\u98791
+- \u5217\u8868\u98792
+- \u5217\u8868\u98793
+
+### \u5C0F\u5C0F\u6807\u9898 1
+
+\u8FD9\u662F\u4E00\u6BB5*\u659C\u4F53*\u6587\u672C\uFF0C\u8FD9\u662F\u4E00\u6BB5**\u7C97\u4F53**\u6587\u672C\u3002
+
+#### \u5C0F\u5C0F\u5C0F\u6807\u9898 1
+
+1. \u6709\u5E8F\u5217\u8868\u98791
+2. \u6709\u5E8F\u5217\u8868\u98792
+3. \u6709\u5E8F\u5217\u8868\u98793
+
+##### \u5C0F\u5C0F\u5C0F\u5C0F\u6807\u9898 1
+
+> \u8FD9\u662F\u4E00\u4E2A\u5F15\u7528\u3002
+
+###### \u6700\u5C0F\u6807\u9898
+
+\`\`\`python
+# \u8FD9\u662F\u4E00\u6BB5\u4EE3\u7801
+print("Hello, Markdown!")
+`,i=n(65273),c=n(48885),t=n(85893),r=function(){return(0,t.jsxs)("div",{className:"page",children:[(0,t.jsx)(i.Z,{}),(0,t.jsxs)("div",{className:"content",children:[(0,t.jsx)("div",{className:"title",children:"Services"}),(0,t.jsx)("div",{className:"markdown",children:(0,t.jsx)(c.UG,{children:e})})]})]})}},49721:function(o){o.exports=JSON.parse('{"name":"Boyin Tan","location":"ShenZhen, China","email":"BoyinTan1221@gmail.com","github":"https://github.com/shuaijiumei","twitter":"https://twitter.com/BoyinTan","googleScholar":"https://scholar.google.com/citations?hl=zh-CN&user=lvhoqu4AAAAJ","showBar":["aboutme","publications","cv"]}')}}]);
